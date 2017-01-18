a9e89db8d6bd1a8cf4782e1601b3c90c1e772471
Lanre Adelowo <me@lanreadelowo.com>
2017-01-18 03:54:43 +0100

    Code style fix plus adding link to codepen

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app) and contains a freecodecamp exercise. I am making use of this to learn ReactJS.
 
-> yarn install
-> yarn run start
+> yarn
+
+> yarn start
+
+
+> Codepen -- http://codepen.io/ladel/full/WRoYLN/
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import LeadershipBoard from "./LeadershipBoard";
-import './index.css';
+import "./index.css";
 
 ReactDOM.render(
-  <LeadershipBoard />,
-  document.getElementById('root')
+    <LeadershipBoard />,
+    document.getElementById('root')
 );
